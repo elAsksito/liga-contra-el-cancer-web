96cfd07d4e734ceeb3911bb2ce5a8cf3f88301e6
elAsksito <xphery035@gmail.com>
2024-10-02 14:55:41 -0500

    Se sube el proyecto Springboot

---
+++ src/main/java/com/ask/ligacontraelcancer/LigacontraelcancerApplication.java
@@ -0,0 +1,13 @@
+package com.ask.ligacontraelcancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigacontraelcancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigacontraelcancerApplication.class, args);
+	}
+
+}
